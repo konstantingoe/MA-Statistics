c266023cb7e84646b4bfe701413dc3d054bce7cc
konstantingoe <konstantingoebler@googlemail.com>
2019-06-29 21:11:16 +0200

    fixed knn imputation

--- bayesian_network.R
+++ bayesian_network.R
@@ -10,7 +10,7 @@ mydata <- import(paste(path, "topwealth_cleaned.dta", sep = "/"))
 mydata <- mydata %>% 
   mutate(lnorbis         = log(orbis_wealth) - log(mean(orbis_wealth, na.rm = T))) %>%
   mutate(lnwage_g        = log(1+wage_gross_m) - log(mean(wage_gross_m, na.rm = T))) %>% 
-  mutate(lnjobduration   = log(jobduration) - mean(log(jobduration), na.rm = T)) %>% 
+  mutate(lnjobduration   = log(jobduration) - log(mean(jobduration, na.rm = T))) %>% 
   mutate(lnsqmtrs        = log(sqmtrs) - log(mean(sqmtrs, na.rm = T))) %>% 
   mutate(lnresidence     = log(residence_value)- log(mean(residence_value, na.rm = T))) %>% 
   mutate(lnestate        = log(other_estate_value)- log(mean(other_estate_value, na.rm = T))) %>% 
@@ -25,11 +25,12 @@ mydata <- mydata %>%
   mutate(lnestatedebt    = log(other_estate_debt)- log(mean(other_estate_debt, na.rm = T))) %>% 
   mutate(lnconsumerdebt  = log(consumer_debt)- log(mean(consumer_debt, na.rm = T))) %>% 
   mutate(lneducationdebt = log(education_debt)- log(mean(education_debt, na.rm = T))) %>% 
-  mutate(lnsqrmtrs       = log(sqmtrs) - mean(log(sqmtrs), na.rm = T)) %>%
-  mutate(lnsaving        = log(1+saving_value) - mean(log(1+saving_value)), na.rm = T) %>%
-  mutate(lnhhnetto       = log(1+hhnetto) - mean(log(1+hhnetto), na.rm = T)) %>%
-  mutate(lninheritance   = log(1+total_inheritance) - mean(log(1+total_inheritance), na.rm = T)) %>%
-  mutate(educationjob   = ifelse(educationjob <0 , NA, educationjob))
+  mutate(lnsqrmtrs       = log(sqmtrs) - log(mean(sqmtrs, na.rm = T))) %>%
+  mutate(lnsaving        = log(1+saving_value) - log(mean(1+saving_value, na.rm = T))) %>%
+  mutate(lnhhnetto       = log(1+hhnetto) - log(mean(1+hhnetto, na.rm = T))) %>%
+  mutate(lninheritance   = log(1+total_inheritance) - log(mean(1+total_inheritance, na.rm = T))) %>%
+  mutate(educationjob   = ifelse(educationjob <0 , NA, educationjob)) 
+
 
 # Bayesian Network for other estate:
   #make sure I have the same variables for BN as for MICE plus a couple more
@@ -40,15 +41,25 @@ mydata <- mydata %>%
   
   
 ### ask Markus for regional indicators!  
+
+preparation <- mydata %>% 
+  mutate(lnresidence = ifelse(is.na(lnresidence) & owner == 0,0,lnresidence)) %>% 
+  mutate(lnassets    = ifelse(is.na(lnassets) & assets_filter == 0,0,lnassets)) %>% 
+  mutate(lnbuilding  = ifelse(is.na(lnbuilding) & building_contract_filter == 0,0,lnbuilding)) %>% 
+  mutate(lnbusiness  = ifelse(is.na(lnbusiness) & business_holdings_filter == 0,0,lnbusiness)) %>% 
+  mutate(lnvehicles  = ifelse(is.na(lnvehicles) & vehicles_filter == 0,0,lnvehicles)) %>% 
+  mutate(lnestatedebt  = ifelse(is.na(lnestatedebt) & other_estate_debt_filter == 0,0,lnestatedebt))
   
+
   
 estatevars <- c("sex", "lnhhnetto", "lnwage_g", "educationjob", "other_estate_debt_filter",
                 "housedebt", "selfempl", "employed", "famstd", "age", "total_inheritance",
                 "lnorbis", "lmstatus", "hours_actual", "inherit_filter", "other_estate_value_limits", "kidsu16",
-                "livingcond", "estate_income_value", "saving_value", "owner", "lnestate", "lninheritance")
+                "livingcond", "estate_income_value", "saving_value", "owner", "lnestate", "other_estate_value", "lninheritance",
+                "lnresidence", "lnassets", "lnbuilding", "lnbusiness", "lnvehicles", "lnestatedebt")
   
   
-estate <- select(filter(mydata, other_estate == 1), one_of(estatevars)) %>% 
+estate <- select(filter(preparation, other_estate == 1), one_of(estatevars)) %>% 
   mutate(lmstatus = factor(lmstatus)) %>% 
   mutate(owner = factor(owner)) %>% 
   mutate(sex = factor(sex)) %>% 
@@ -70,43 +81,44 @@ gg_miss_var(estate, show_pct = TRUE)
 
 #### Quick imputation for simulation: kNN ####
 
-estate.clean <- VIM::kNN(estate, addRF = T, imp_var=F)
+estate.clean <- VIM::kNN(select(estate, -other_estate_value_limits), addRF = T, imp_var=F)
+estate_limits <- estate$other_estate_value_limits
+estate.clean <- cbind(estate.clean, estate_limits)
 
+estate.clean <- estate.clean %>% 
+  mutate(estate = exp(lnestate)* mean(estate$other_estate_value, na.rm = T)) %>% 
+  mutate(estate_limits = ifelse(is.na(estate_limits) & estate < 20000,1,
+                                            ifelse(is.na(estate_limits) & estate >= 20000 & estate < 150000,2,
+                                                   ifelse(is.na(estate_limits) & estate >= 150000 & estate < 500000,3,
+                                                          ifelse(is.na(estate_limits) & estate >= 500000 & estate < 1000000,4,
+                                                                 ifelse(is.na(estate_limits) & estate >= 1000000,5,estate_limits))))))
+
+
+estate.clean <- select(estate.clean, -other_estate_value, -estate)
 anyNA(estate.clean)
 gg_miss_var(estate.clean, show_pct = TRUE)
 
-estate.bn <- VIM::kNN(estate, addRF = T, imp_var=F)
+#estate.bn <- discretize(estate.bn, ordered = T, method = "interval")
+#learned <- hc(estate.bn)
+#modelstring(learned)
+#score(learned, data = estate.bn, type = "bic")
 
-estate.bn <- discretize(estate.bn, ordered = T, method = "interval")
-learned <- hc(estate.bn)
-modelstring(learned)
-score(learned, data = estate.bn, type = "bic")
-
-learned2 <- hc(estate.bn, score = "bde")
-arc.strength(learned, data = estate.bn, criterion = "bic")
+#learned2 <- hc(estate.bn, score = "bde")
+#arc.strength(learned, data = estate.bn, criterion = "bic")
 # from the learned score, removing any will result in a decrease of BIC
 
 learned3 <- tabu(estate.clean, score = "bic-cg")
 
-
-filehead <- c(T,F,F,T,T,T,T,T,T,F,F,F,T,F,T,T,T,T,F,F,T,F,F)
-varnames <- names(estate.bn)
-cardinality <- c(2,50,50,4,2,2,2,2,4,5,50,50,5,50,2,5,2,5,50,50,2,50,50)
-
-
-bndata <- BNDataset(data = data.matrix(estate.bn, rownames.force = NA), discreteness = filehead, variables = varnames, node.sizes = cardinality)
-
-net <- learn.network(bndata, use.imputed.data = F,
-                     initial.network = "random.chain",
-                     seed = 12345)
 pdf("learnedBN.pdf") 
 graphviz.plot(learned3)
 dev.off()
 
-graphviz.plot(learned2)
+#graphviz.plot(learned2)
 
 mb(learned3, node = "lnestate")
 narcs(learned3)
+directed.arcs(learned3)
+nnodes(learned3)
 directed.arcs(learned3)
 
 hist(estate.clean$lnestate, freq=F, breaks=25)
@@ -118,6 +130,11 @@ hist(estate.clean$lnhhnetto, freq=F, breaks=15)
 lines(density(estate.clean$lnhhnetto), col="red")
 lines(seq(-8, 4, by=.1), dnorm(seq(-8, 4, by=.1),
                                mean(estate.clean$lnhhnetto), sd(estate.clean$lnhhnetto)), col="blue")
+
+
+
+
+
 
 ##### learn structure
 
@@ -154,7 +171,16 @@ bn <- learn.network(bndata,
 
 
 
+filehead <- c(T,F,F,T,T,T,T,T,T,F,F,F,T,F,T,T,T,T,F,F,T,F,F)
+varnames <- names(estate.bn)
+cardinality <- c(2,50,50,4,2,2,2,2,4,5,50,50,5,50,2,5,2,5,50,50,2,50,50)
 
+
+bndata <- BNDataset(data = data.matrix(estate.bn, rownames.force = NA), discreteness = filehead, variables = varnames, node.sizes = cardinality)
+
+net <- learn.network(bndata, use.imputed.data = F,
+                     initial.network = "random.chain",
+                     seed = 12345)
 
 
 
